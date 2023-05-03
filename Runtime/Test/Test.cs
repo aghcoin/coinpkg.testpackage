c08408fafb31fcec8ac4ea4df8efad8629fb6146
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-05-03 14:05:48 +0200

    Assembly test

--- Runtime/Test/Test.cs
+++ Runtime/Test/Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Test {
+namespace CoinPackage.Test {
     public static class Test
     {
         public static void Print(string msg) {
