84e322ac495e75fa5be9423deb348b9bd5997d49
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-05-03 13:19:48 +0200

    First release

---
+++ Runtime/Test/Test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test {
+    public static class Test
+    {
+        public static void Print(string msg) {
+            Debug.Log("Test Package: " + msg);
+        }
+    }
+}
+
+
